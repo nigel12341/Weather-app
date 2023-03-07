ef14636cf51b947f2cf57c843372bb3faf2d7666
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-03-07 22:44:54 +0100

    Add springboot

---
+++ weather backend/src/test/java/nl/nigelchristiaans/weatherbackend/WeatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.nigelchristiaans.weatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
