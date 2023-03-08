4b0051839776a2655a088351ac09f0157680c9ad
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-03-08 17:51:24 +0100

    Added a today weather box

---
+++ src/components/WeatherBoxToday.vue
@@ -0,0 +1,142 @@
+<template>
+<section class="container card">
+<h3>The weather today in {{ location.name }}, {{location.region}}</h3>
+  <h1 class="temperatureDisplay">{{weatherMessage.temp_c}}°</h1>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+        <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+        Wind: {{weatherMessage.wind_kph}} km/h {{weatherMessage.wind_dir}}</li>
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet-half" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10c0 0 2.5 1.5 5 .5s5-.5 5-.5c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
+          <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"/>
+        </svg>
+        Humidity: {{weatherMessage.humidity}}%</li>
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-half" viewBox="0 0 16 16">
+          <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V6.5a.5.5 0 0 1 1 0v4.585a1.5 1.5 0 0 1 1 1.415z"/>
+          <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1z"/>
+        </svg>
+        Feels like: {{weatherMessage.feelslike_c}}°C</li>
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun" viewBox="0 0 16 16">
+          <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+        </svg>
+        UV: {{weatherMessage.uv}} out of 10</li>
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-collapse" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8Zm7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0Zm-.5 11.707-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0v-3.793Z"/>
+        </svg>
+        Pressure: {{weatherMessage.pressure_mb}} mb</li>
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-fog2" viewBox="0 0 16 16">
+          <path d="M8.5 4a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 13H.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 4zM0 8.5A.5.5 0 0 1 .5 8h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+        Visibility: {{weatherMessage.vis_km}} km</li>
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-rain-heavy" viewBox="0 0 16 16">
+          <path d="M4.176 11.032a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 1 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm3 0a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 1 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm3 0a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 1 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm3 0a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 0 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293zm.229-7.005a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10H13a3 3 0 0 0 .405-5.973zM8.5 1a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 1z"/>
+        </svg>
+        Rain: {{weatherMessage.precip_mm}} mm</li>
+      <li class="list-group-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
+          <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
+        </svg>
+        Moon Phase: {{astroData.moon_phase}}
+      </li>
+      <li class="list-group-item"></li>
+    </ul>
+</section>
+</template>
+
+<script lang="ts">
+export default {
+  name: "WeatherBoxToday",
+  mounted() {
+    this.getWeatherMessage();
+    this.getAstronomyData()
+
+  },
+  data() {
+    return {
+      weatherMessage: '',
+      location: '',
+      ip: '',
+      astroData: ''
+    }
+  },
+  methods: {
+    async getIpAdress() {
+      let response = await fetch('https://api.ipify.org?format=json');
+      let data = await response.json();
+      return data.ip
+    },
+
+    async getWeatherMessage() {
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          await fetch(import.meta.env.VITE_URL_API + '/v1/weather/current?' + new URLSearchParams({
+            location: position.coords.latitude + ',' + position.coords.longitude
+          })).then(response => response.json()).then(data => {
+            this.weatherMessage = data.current
+            this.location = data.location
+          })
+        }, async () => {
+
+          await fetch(import.meta.env.VITE_URL_API + '/v1/weather/current?' + new URLSearchParams({
+            location: await this.getIpAdress(),
+          })).then(response => response.json()).then(data => {
+            this.weatherMessage = data.current;
+            this.location = data.location
+          })
+        });
+      }
+    },
+
+    async getAstronomyData() {
+      const date = new Date();
+
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+
+      // This arrangement can be altered based on how we want the date's format to appear.
+      const currentDate = `${year}-${month}-${day}`;
+
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          await fetch(import.meta.env.VITE_URL_API + '/v1/astronomy?' + new URLSearchParams({
+            location: position.coords.latitude + ',' + position.coords.longitude,
+            date: currentDate
+          })).then(response => response.json()).then(data => {
+            this.astroData = data.astronomy.astro
+          })
+        }, async () => {
+          await fetch(import.meta.env.VITE_URL_API + '/v1/astronomy?' + new URLSearchParams({
+            location: await this.getIpAdress(),
+            date: currentDate
+          })).then(response => response.json()).then(data => {
+            this.astroData = data.astronomy.astro
+          })
+        });
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: left;
+
+}
+.temperatureDisplay {
+  font-size: 4rem;
+}
+ul{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr)
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
     <ShortWeatherMessage/>
+  <WeatherBoxToday/>
     <SevenDayForecast/>
 </template>
 
 <script lang="ts">
 import ShortWeatherMessage from "../components/ShortWeatherMessage.vue";
 import SevenDayForecast from "../components/SevenDayForecast.vue";
+import WeatherBoxToday from "../components/WeatherBoxToday.vue";
 export default {
   name: "Home",
-  components: {SevenDayForecast, ShortWeatherMessage}
+  components: {WeatherBoxToday, SevenDayForecast, ShortWeatherMessage}
 }
 </script>
